7897b55057b0c9255313d83e82a5cd8a45410e06
Giancarlo <gmatutecr@gmail.com>
2018-12-12 19:34:40 -0600

    Building basic scaffold

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,19 @@
 class PeopleController < ApplicationController
+  def create
+  end
+    
+  def new 
+  end
+  
+  def edit
+  end
+  
+  def show
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
 end
