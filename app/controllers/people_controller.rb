2b3cb7e2e0160a41af92063114959250b6b3ca09
Giancarlo <gmatutecr@gmail.com>
2018-12-12 20:18:16 -0600

    add people scaffold

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,19 +1,62 @@
 class PeopleController < ApplicationController
-  def create
+  before_action :set_person, only: [:show, :edit, :update, :destroy]
+  def index
+    @people = Person.all
   end
-    
-  def new 
+
+  def create
+    @person = Person.new(person_params)
+
+    respond_to do |format|
+      if @person.save
+        format.html { redirect_to @person, notice: 'person was successfully created.' }
+        format.json { render :show, status: :created, location: @person }
+      else
+        format.html { render :new }
+        format.json { render json: @person.errors, status: :unprocessable_entity }
+      end
+    end
   end
   
+  def new 
+    @person = Person.new
+  end
+
   def edit
   end
-  
+
   def show
   end
-  
+
   def update
+    respond_to do |format|
+      if @person.update(person_params)
+        format.html { redirect_to @person, notice: 'person was successfully updated.' }
+        format.json { render :show, status: :ok, location: @person }
+      else
+        format.html { render :edit }
+        format.json { render json: @person.errors, status: :unprocessable_entity }
+      end
+    end
   end
-  
+
   def destroy
+    @person.destroy
+    respond_to do |format|
+      format.html { redirect_to people_url, notice: 'person was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_person
+    @person = Person.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def person_params
+    params.require(:person).permit(:first_name, :last_name, :email, :job, :bio, :gender, :birthdate)
+  
   end
 end
