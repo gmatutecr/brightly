10551e82e088472ee961fbe0ba4e699a75d32330
Giancarlo <gmatutecr@gmail.com>
2018-12-14 00:16:00 -0600

    ux improvements

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,14 @@
 module PeopleHelper
+  def display_gender(gender)
+    case gender
+      when 0
+        "Not known"
+      when 1
+        "Male"
+      when 2
+        "Female"
+      when 9
+        "not applicable"
+    end
+  end  
 end
